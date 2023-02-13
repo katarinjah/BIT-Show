6e558c893adf0de0199956827c5246fcbb93a092
katarinjah <katarina.d.262@gmail.com>
2023-02-14 00:21:20 +0100

    Worked on rendering single show info page

--- BIT-Show/js/app.js
+++ BIT-Show/js/app.js
@@ -1,40 +1,83 @@
-// render top 50 shows on page load
+// render single show info page
 
 $(function() {
 
-    $.ajax({
-        url: "https://api.tvmaze.com/shows",
-      })
-      .done((data) => {
-        const sortedData = data.sort((a, b) => {
-          return b.rating.average - a.rating.average;
-        });
-      
-        const top50 = sortedData.slice(0, 50);
-        
-        top50.forEach((show) => {
-            const showDiv = $("<div>", {
-              class: "col-sm-12 col-md-6 col-lg-4 col-xl-4 d-flex justify-content card",
-              style: "width: 18rem;"
-            });
-            const cardBody = $("<div>", {
-                class: "card-body"
-            });
-            const showImage = $("<img>", {
-              src: show.image.medium,
-              class: "card-img-top",
-              alt: show.name,
-              //title: show.name
-            });
-            const showName = $("<h5>").text(show.name);
+    $(document).on("click", "div.singleCard", function() {
+        const rowCards = $("div.cards");
+        const mainTitle = $("#main-title");
+        mainTitle.hide();
+        rowCards.hide();
+        const showId = $(this).data("id");
+    
+        $.ajax({
+            url: `https://api.tvmaze.com/lookup/shows/${showId}`,
+        })
             
-            cardBody.append(showImage, showName);
-            showDiv.append(cardBody);
-            const rowCards = $("div.cards");
-            rowCards.append(showDiv);
+        .done((showData) => {
+            const showName = showData.name;
+            const showPoster = showData.image.medium;
+            const showDescription = showData.summary;
+            const numberOfSeasons = showData.seasons.length;
+        
+            const seasons = showData.seasons.map((season) => {
+                return {
+                    startDate: season.premiereDate,
+                    endDate: season.endDate,
+                };
+            });
+        
+            const cast = showData.cast.map((actor) => {
+                return actor.person.name;
+            });
+        
+            const showFeatures = showData.features;
+            const showCrew = showData.crew;
+            const showAkas = showData.akas;
+            const showEpisodes = showData._embedded.episodes;
+        
+            const mainDiv = $("div.main");
+            mainDiv.empty();
+        
+            const nameDiv = $("<h4>").addClass("name").text(showName);
+            mainDiv.append(nameDiv);
+        
+            const posterImg = $("<img>").attr("src", showPoster);
+            mainDiv.append(posterImg);
+        
+            const descriptionDiv = $("<div>").addClass("description").text(showDescription);
+            mainDiv.append(descriptionDiv);
+        
+            const seasonsDiv = $("<div>").addClass("seasons");
+            seasons.forEach((season) => {
+                const start = season.startDate;
+                const end = season.endDate;
+                const seasonDiv = $("<div>").text(`Season start: ${start}, Season end: ${end}`);
+                seasonsDiv.append(seasonDiv);
+            });
+            mainDiv.append(seasonsDiv);
+        
+            const castDiv = $("<div>").addClass("cast");
+            cast.forEach((actor) => {
+                const actorDiv = $("<div>").text(actor);
+                castDiv.append(actorDiv);
+            });
+            mainDiv.append(castDiv);
+        
+            const featuresDiv = $("<div>").addClass("features").text(showFeatures);
+            mainDiv.append(featuresDiv);
+        
+            const crewDiv = $("<div>").addClass("crew").text(showCrew);
+            mainDiv.append(crewDiv);
+        
+            const akasDiv = $("<div>").addClass("akas").text(showAkas);
+            mainDiv.append(akasDiv);
+
+            const episodesDiv = $("<div>").addClass("episodes");
+            showEpisodes.forEach((episode) => {
+                const episodeDiv = $("<div>").text(episode.name); 
+                mainDiv.append(episodeDiv);
+            });
         });
     });
-      
-});
-    
 
+})
--- BIT-Show/js/ui.js
+++ BIT-Show/js/ui.js
@@ -1,2 +1,42 @@
+// render top 50 shows on page load
 
+$(function() {
+
+    $.ajax({
+        url: "https://api.tvmaze.com/shows",
+      })
+      .done((data) => {
+        const sortedData = data.sort((a, b) => {
+          return b.rating.average - a.rating.average;
+        });
+      
+        const top50 = sortedData.slice(0, 50);
+        
+        top50.forEach((show) => {
+            const showDiv = $("<div>", {
+              class: "col-12 col-md-6 col-lg-4 d-flex justify-content singleCard",
+              style: "width: 18rem;"
+            });
+            const cardBody = $("<div>", {
+                class: "card-body"
+            });
+            const showImage = $("<img>", {
+              src: show.image.medium,
+              class: "card-img-top",
+              alt: show.name,
+              //title: show.name
+            });
+            const showName = $("<h5>").text(show.name);
+            
+            cardBody.append(showImage, showName);
+            showDiv.append(cardBody);
+            const rowCards = $("div.cards");
+            rowCards.append(showDiv);
+        });
+    });
+      
+});
+
+
+    
 
